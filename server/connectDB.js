054d7b4acaa3205e4e78fee3ce57dea1d97890d1
Z <florian.pendaries@ynov.com>
2018-05-22 16:24:23 +0200

    [Fix Struct] Fix Bin/www pz

--- server/connectDB.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/projetdevB3', function(err) {
-  if (err) { throw err; }
-  else{
-      console.log("database Connected");
-  }
-});
